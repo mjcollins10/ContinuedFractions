310c1b77dc5662b3188793202755ace90c8ab5ac
User <user@Users-MacBook-Pro.local>
2024-10-22 21:55:37 -0600

    Fix bug in ingestY

--- gosper.hs
+++ gosper.hs
@@ -12,19 +12,19 @@ evalCF k (a:as) | k > 0     = a + 1/(evalCF (k-1) as)
  - Gosper's algorithm for arithmetic on pairs of CFs
  -}
 
-infinity :: Num a => a
 infinity = 999999999999999999
-noOutput :: Num a => a
 noOutput = -99999999999999999
 
--- find min,max of homomorphic matrix (axy + bx +cy + d)/(exy + fx + gy + h)
--- as x,y range from zero to infinity
+-- range_ is min,max of integer part of homomorphic matrix (axy + bx +cy + d)/(exy + fx + gy + h)
+-- as x,y vary independently from zero to infinity
 mightBeInf (n,d) = d==0 && n/=0
+allZero curMatrix = all (all (==0)) curMatrix
 openceil p r = (div p r) + if (mod p r) == 0 then -1 else 0 -- if asymptotic upper bound is k, integer part is <= k-1
 range_ [numr,denr] | any (>0) denr && any (<0) denr && any (/= 0) numr = (-999999999,999999999) -- sign change in denominator
                    | any mightBeInf [ (n,d) | (n,d) <- zip numr denr ] = (-999999999,999999999) -- infinite upper bound
-                   | otherwise = ( fromIntegral (minimum [ (div n d)      | (n,d) <- zip numr denr , d /= 0 ]),
-                                  fromIntegral (maximum [ (openceil n d) | (n,d) <- zip numr denr , d /= 0]) )
+                   | allZero [numr,denr]                               = (-999999999,999999999) -- end of computation
+                   | otherwise = ( fromIntegral (minimum [ (div n d)      | (n,d) <- zip numr denr , d /= 0]),
+                                   fromIntegral (maximum [ (openceil n d) | (n,d) <- zip numr denr , d /= 0]) )
 
 -- after ingesting leading terms, can assume all terms >= 1
 -- easier to  map xx, yy = x-1, y-1 and find range assuming xx,yy>0
@@ -33,10 +33,12 @@ range [num, den] = range_ [shift num , shift den]
 
 -- transform homomorphic matrix (axy + bx +cy + d)/(exy + fx + gy + h)
 -- by substitution x <- n + 1/x or y <- n + 1/y
-ingestX n [[a,b,c,d],[e,f,g,h]] | n==infinity = [[0,0,a,b],[0,0,e,f]]
-                                | otherwise   = [[n*a+c, n*b+d, a, b], [n*e+g, n*f+h, e, f]]
-ingestY n [[a,b,c,d],[e,f,g,h]] | n==infinity = [[0,a,0,c],[0,e,0,g]]
-                                | otherwise   = [[n*a+b, a, n*c+d, c], [n*e+f, e, n*g+h, g]]
+ingestX n [[a,b,c,d],[e,f,g,h]] | all (==0) [a,b,e,f] = [[0,0,c,d],[0,0,g,h]] 
+                                | n==infinity         = [[0,0,a,b],[0,0,e,f]]
+                                | otherwise           = [[n*a+c, n*b+d, a, b], [n*e+g, n*f+h, e, f]]
+ingestY n [[a,b,c,d],[e,f,g,h]] | all (==0) [a,c,e,g] = [[0,b,0,d],[0,f,0,h]]
+                                | n==infinity         = [[0,a,0,c],[0,e,0,g]]
+                                | otherwise           = [[n*a+b, a, n*c+d, c], [n*e+f, e, n*g+h, g]]
 
 -- return (numerator/denominator - bn)^{-1}
 produce bn [ numerator, denominator ] = [denominator, [j-bn*k | (j,k) <- zip numerator denominator]]
@@ -51,10 +53,10 @@ tail_ xs = tail xs
 -- produce the next term of output if we can, otherwise read another term of input
 -- current state is ( (unread part of x, unread part of y, history), (current homomorphic matrix, last output term) )
 -- most recent history is at head of h
-allZero curMatrix = maximum (map maximum curMatrix) == 0 && minimum (map minimum curMatrix) == 0 
 gosper ((x,y,h),(curM,bn))  | allZero curM      = ((x,y,h), (curM, infinity)) -- stop on all-zero state
+                            | all null [x,y]    = ((x,y,h), (curM, infinity)) -- nothing more to do
                             | low == hi         = ((x,y,[h!!0]), (produce hi curM, hi)) -- produce another term of output
-                            | length h > 100    = (([],[],"x"), (produce hi curM, hi)) -- stuck in loop, truncate
+                            | length h > 100    = ((x,y,"x"), (produce hi curM, hi)) -- stuck in loop, truncate
                             | h!!0 == 'y'       = ((tail_ x,y,'x':h), (ingestX (head_ x) curM, noOutput)) -- get next x
                             | otherwise         = ((x,tail_ y,'y':h), (ingestY (head_ y) curM, noOutput)) -- get next y
                                   where (low,hi)  = range curM
@@ -67,22 +69,14 @@ notInf      (_, (curM, bn)) = bn /= infinity
 arithCF initialState x y = map (fromIntegral.snd.snd) (takeWhile notInf ( filter outputState (arithCF_ x y initialState) ))
 
 -- matrix initializations for arithmetic
-initAdd :: Num a => [[a]]
 initAdd = [[0,1,1,0],[0,0,0,1]]
-initSub :: Num a => [[a]]
 initSub = [[0,1,-1,0],[0,0,0,1]]
-initMul :: Num a => [[a]]
 initMul = [[1,0,0,0],[0,0,0,1]]
-initDiv :: Num a => [[a]]
 initDiv = [[0,1,0,0],[0,0,1,0]]
 
-addCF :: (Integral a, Num b) => [a] -> [a] -> [b]
 addCF = arithCF initAdd
-subCF :: (Integral a, Num b) => [a] -> [a] -> [b]
 subCF = arithCF initSub
-mulCF :: (Integral a, Num b) => [a] -> [a] -> [b]
 mulCF = arithCF initMul
-divCF :: (Integral a, Num b) => [a] -> [a] -> [b]
 divCF = arithCF initDiv
 
 {-
@@ -94,7 +88,7 @@ fromCF :: CF -> [Integer]
 fromCF (MakeCF terms) = terms
 
 instance Num CF where
-  fromInteger n = MakeCF [n]
+  fromInteger n  = MakeCF [n]
   x + y = MakeCF (addCF (fromCF x) (fromCF y))
   x - y = MakeCF (subCF (fromCF x) (fromCF y))
   x * y = MakeCF (mulCF (fromCF x) (fromCF y))
@@ -106,6 +100,7 @@ instance Num CF where
 
 instance Fractional CF where
   x / y  = MakeCF (divCF (fromCF x) (fromCF y))
+  fromRational x = MakeCF (float2cf x)
 
 instance Eq CF where
   x == y = and (take 100 compxy)
@@ -119,6 +114,8 @@ instance Ord CF where
 instance Show CF where
   show x = show (take 8 (fromCF x))
 
+showNterms n (MakeCF terms) = take n terms
+
 -- examples for testing
 sqrt2 = MakeCF (1:(cycle [2]))
 -- "[0..]" has to be an Enum type
@@ -131,3 +128,4 @@ gamma = MakeCF [0, 1, 1, 2, 1, 2, 1, 4, 3, 13, 5, 1, 1, 8, 1, 2, 4, 1, 1, 40, 1,
 -- for debugging: make intermediate states printable
 hist (_,_,h) = h
 viewState (h,m) = (hist h, fst m, range (fst m), snd m)
+
