1d1634748205c76bb1f09e62cea8b939da4dab83
User <user@Users-MacBook-Pro.local>
2025-02-28 14:12:58 -0700

    Fix stalling bug

--- gosper.hs
+++ gosper.hs
@@ -3,6 +3,7 @@
  - with modifications to avoid infinite loops
  -}
 
+import Text.Printf
 import Data.Ratio -- get (%) operator for rational arithmetic
 termToBound n =  (n%1, (n+1)%1) -- generalized internal representation of CF term n
 -- a bound of the form (n,n+1) gives us n as the next CF term
@@ -253,39 +254,63 @@ fixpoint_ mat guess | mfloor == guess || mceil == guess = mfloor
 
 -- rational estimates of fixpoint
 -- binary search until | y - M(y) | < epsilon
+-- splitting binary search with mediant or average leads to very large numerators and denominators
+-- so we instead find the simplest dyadic rational in the search interval
 fixpointRat_ eps mat guess | abs (guess - mval) < eps = (min guess mval, max guess mval)
-                           | otherwise                = fixpointRat_ eps mat (mediant guess mval)
+                           | otherwise                = fixpointRat_ eps mat (dyadicBetween guess mval)
                                 where mval = evalMatRat mat guess
 
+-- return a range (lo,hi) which contains the fixed point of (px+q)/(rx+s), with hi - lo < eps
 fixpointRat :: Integral a => Ratio a -> [[a]] -> (Ratio a, Ratio a)
 fixpointRat eps [[p,q],[r,s]] = fixpointRat_  eps [[p,q],[r,s]] firstGuess
-                                  where firstGuess = if r > 0 then max 1 (1 + (-s%r)) else (1%1) 
+                                  where firstGuess = ((abs p)+(abs q))%1-- if r > 0 then max 1 (1 + (-s%r)) else (1%1) 
 
--- splitting binary search with (lo + hi)/2 leads to very large numerators and denominators
--- in fact still getting larger terms than we expected
-mediant lo hi = ((numerator lo)+(numerator hi))%((denominator lo)+(denominator hi))
+isint rat = (denominator rat) == 1
+dyadicBetween lo hi | hi < lo = dyadicBetween hi lo
+                    | (ceiling lo) <  (floor hi) = integerBetween (ceiling lo) (floor hi)
+                    | (ceiling lo) == (floor hi) = eqCase lo hi
+                    | otherwise                  = ((floor lo)%1) + (between lo_ hi_)
+                        where lo_ = lo - (fromIntegral (floor lo))
+                              hi_ = hi - (fromIntegral (floor lo))
 
--- TODO: finish fix for stalling bug; output ambiguous terms of (sqrt x)
+eqCase lo hi | not (isint lo || isint hi) = (floor hi)%1 -- uninque integer strictly between
+             | isint lo  = lo + (approxDiff (hi - lo))
+             | otherwise = hi - (approxDiff (hi - lo)) -- isint hi
+-- dyadic approximation; returns something positive and strictly less than delta
+approxDiff delta = if delta > (1%2) then (1%2) else (approxDiff (2*delta))/2
+
+-- called only with lo < hi and 2 or more integers between
+integerBetween lo hi | hi == lo + 1 = (lo%1) + (1%2)
+                     | otherwise    = (floor ((lo+hi)%2))%1
+
+-- lo, hi are in the interval [0,1]; find dyadic rational strictly between
+-- proof of termination: difference between arguments doubles on each recursive call
+between lo hi | lo < (1%2) && hi > (1%2) = (1%2)
+              | hi <= (1%2) = (between (2*lo) (2*hi))/2
+              | otherwise = (1%2) + (between (2*lo-1) (2*hi-1))/2
+
 -- will always need to ingest first term of x, so just do it
--- cfSqrt (MakeCF_ x) =  MakeCF_ $ cfSqrtIter ( ingestX (head_ x) ( [[0,1,0,0], [0,0,1,0]], nobound, nobound ) ) (tail_ x) 
+cfSqrt (MakeCF_ x) =  MakeCF_ $ cfSqrtIter ( ingestX (head_ x) ( [[0,1,0,0], [0,0,1,0]], nobound, nobound ) ) (tail_ x) 
 -- advance to next stage of square-root computation
 -- if floor of fixed point of mat does not depend on x, we can get next
 -- term of output; else ingest another term of x into mat
-{- cfSqrtIter mat x | all (==0) (m!!1) = [] -- have reached end of computing rational result
+-- must output ambiguous terms to prevent stalling (see cfAlgorithm.tex)
+cfSqrtIter mat x | all (==0) (m!!1) = [] -- have reached end of computing rational result
                  | y <= 0 = []
                  | matHasFixpoint = (termToBound y):(cfSqrtIter (produce y (ingestY (termToBound y) mat)) x) 
-                 | otherwise      = yBounds:cfSqrtIter (ingestX (head_ x) mat) (tail_ x)) -- output ambiguous term
+                 | otherwise      = yBounds:(cfSqrtIter (ingestX (head_ x) mat) (tail_ x)) -- output ambiguous term
                       where y   = fixpoint (fixvar m xLo)
-                            yHi = fixpoint (fixvar m xHi) -- change to just check if y is fixpoint
-                            matHasFixpoint = y == yHi
-                            yBounds =  getbounds (fixpointRat eps (fixvar m xLo), fixpointRat eps  (fixvar m xLo)) 
-                            eps = matchAccuracyOf boundX -- just min 1 (xHi-xLo) 
-                            -- true fixpoint can be irrational, need rational bounds
-                            -- but must be tight enough to guarantee convergence!!!
-                            -- 'getbounds' will grab lower bound of smaller, upper bound of larger
-                            -- (just take overall min, max of the four values)
+                            yHi = fixpoint (fixvar m xHi) 
                             (m, boundX, boundY) = mat
                             (xLo, xHi) = boundX
+                            matHasFixpoint = y == yHi
+                            -- if floor of fixpoint is undetermined, generate ambiguous terms
+                            eps = min (1%3) (xHi - xLo) -- match accuracy of boundX 
+                            yBounds =  getBounds (fixpointRat eps (fixvar m xLo)) (fixpointRat eps  (fixvar m xHi)) 
+                            -- true fixpoint can be irrational, need rational bounds: tight enough to always converge
+                            -- so match accuracy of boundX
 
-reorder (a,b) = if a <= b then (a,b) else (b,a)
--}
+-- negative fixpoint means y -> \infty as x -> \infty; see cfAlgorithm.tex
+getBounds (w,x) (y,z) = if minAll > 0 then (minAll, maxAll) else (maxAll, infinity%1)
+                          where (minAll, maxAll) = (min w y, max x z) -- the normal case
+
